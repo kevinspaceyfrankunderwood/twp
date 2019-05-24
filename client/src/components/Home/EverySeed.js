00a9df3101727164a02a8d56f2fb62087245aef9
Matthew Peterson <mbpeterson19@gmail.com>
2019-05-24 09:46:17 -0600

    updates to service worker

--- client/src/components/Home/EverySeed.js
+++ client/src/components/Home/EverySeed.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-import { TWPHeader, TWPDiv, TWPImage, media, TWPSectionHeader } from '../../styles/GenericStyledComponents';
-import TWPStyleGuide from '../../styles/TWPStyleGuide';
+import {
+  TWPHeader,
+  TWPDiv,
+  TWPImage,
+  media,
+  TWPSectionHeader
+} from '../../styles/GenericStyledComponents'
+import TWPStyleGuide from '../../styles/TWPStyleGuide'
 
 const ResponsiveHeader = TWPSectionHeader.extend`
 	width: 75%;
@@ -16,54 +22,82 @@ const ResponsiveHeader = TWPSectionHeader.extend`
   `} ${media.phone`
     width: 95%;
   `};
-`;
+`
 
-const DisappearingDiv = TWPDiv.extend`@media (max-width: 605px) {display: none;}`;
+const DisappearingDiv = TWPDiv.extend`
+  @media (max-width: 605px) {
+    display: none;
+  }
+`
 
-const DisappearingFirstDiv = TWPDiv.extend`@media (max-width: 907px) {display: none;}`;
+const DisappearingFirstDiv = TWPDiv.extend`
+  @media (max-width: 907px) {
+    display: none;
+  }
+`
 
-const SixHundredFiveDiv = TWPDiv.extend`@media (max-width: 605px) {display: none;}`;
+const SixHundredFiveDiv = TWPDiv.extend`
+  @media (max-width: 605px) {
+    display: none;
+  }
+`
 
 export default class EverySeed extends React.Component {
-	state = { photos: [] };
+  state = { photos: [] }
 
-	componentDidMount() {
-		axios
-			.get('/api/instagram/tenGroup')
-			.then((res) => this.setState({ photos: res.data.data }))
-			.catch((res) => console.log(res));
-	}
+  componentDidMount() {
+    axios
+      .get('/api/instagram/tenGroup')
+      .then(res => this.setState({ photos: res.data.data }))
+      .catch(res => console.log(res))
+  }
 
-	displayPhotos = () => {
-		console.log('here');
-		const first = Math.floor(Math.random() * 3) + 1;
-		const second = Math.floor(Math.random() * 3) + 4;
-		const third = Math.floor(Math.random() * 3) + 7;
-		console.log(this.state.photos[first]);
-		return (
-			<TWPDiv width={'90%'} flexDirection={'row'} flexWrap={'wrap'} padding={'4% 2%'}>
-				<DisappearingDiv width={'300px'} height={'300px'} minWidth={'250px'} margin={'5px'} padding={'0'}>
-					<TWPImage src={this.state.photos[first].images.standard_resolution.url} />
-				</DisappearingDiv>
-				<DisappearingFirstDiv width={'300px'} height={'300px'} minWidth={'250px'} margin={'5px'} padding={'0'}>
-					<TWPImage src={this.state.photos[second].images.standard_resolution.url} />
-				</DisappearingFirstDiv>
-				<TWPDiv width={'300px'} height={'300px'} minWidth={'250px'} margin={'5px'} padding={'0'}>
-					<TWPImage src={this.state.photos[third].images.standard_resolution.url} />
-				</TWPDiv>
-			</TWPDiv>
-		);
-	};
+  displayPhotos = () => {
+    const first = Math.floor(Math.random() * 3) + 1
+    const second = Math.floor(Math.random() * 3) + 4
+    const third = Math.floor(Math.random() * 3) + 7
+    return (
+      <TWPDiv width={'90%'} flexDirection={'row'} flexWrap={'wrap'} padding={'4% 2%'}>
+        <DisappearingDiv
+          width={'300px'}
+          height={'300px'}
+          minWidth={'250px'}
+          margin={'5px'}
+          padding={'0'}
+        >
+          <TWPImage src={this.state.photos[first].images.standard_resolution.url} />
+        </DisappearingDiv>
+        <DisappearingFirstDiv
+          width={'300px'}
+          height={'300px'}
+          minWidth={'250px'}
+          margin={'5px'}
+          padding={'0'}
+        >
+          <TWPImage src={this.state.photos[second].images.standard_resolution.url} />
+        </DisappearingFirstDiv>
+        <TWPDiv
+          width={'300px'}
+          height={'300px'}
+          minWidth={'250px'}
+          margin={'5px'}
+          padding={'0'}
+        >
+          <TWPImage src={this.state.photos[third].images.standard_resolution.url} />
+        </TWPDiv>
+      </TWPDiv>
+    )
+  }
 
-	render() {
-		return (
-			<TWPDiv justifyContent={'space-evenly'} padding={'0'}>
-				<ResponsiveHeader color={TWPStyleGuide.color.darkOrange} padding={'30px 10px'}>
-					The Wildflower Project's focus is to serve as environmental and community stewards, fulfilling our promise of
-					planting with a purpose.
-				</ResponsiveHeader>
-				{this.state.photos.length && this.displayPhotos()}
-			</TWPDiv>
-		);
-	}
+  render() {
+    return (
+      <TWPDiv justifyContent={'space-evenly'} padding={'0'}>
+        <ResponsiveHeader color={TWPStyleGuide.color.darkOrange} padding={'30px 10px'}>
+          The Wildflower Project's focus is to serve as environmental and community
+          stewards, fulfilling our promise of planting with a purpose.
+        </ResponsiveHeader>
+        {this.state.photos.length && this.displayPhotos()}
+      </TWPDiv>
+    )
+  }
 }
