fab08742b58a344c82f523c233436864a8dc0d9a
Matthew Peterson <mbpeterson19@gmail.com>
2019-06-12 12:15:33 -0600

    removed megan

--- client/src/assets/strings/Team.js
+++ client/src/assets/strings/Team.js
@@ -10,13 +10,15 @@ export const employees = [
   {
     fullName: 'Matt Wildenauer',
     title: 'Founder & Landscape Architect',
-    longBio: 'Matt is a professional landscape designer and photographer. He received a B.S. in Horticulture and B.D.S. in Landscape Design from the University of Minnesota. An avid gardener with an eye for design, Matt is driven by an instinct to leave the world in better shape than he found it.',
-    photo: Wildenauer 
+    longBio:
+      'Matt is a professional landscape designer and photographer. He received a B.S. in Horticulture and B.D.S. in Landscape Design from the University of Minnesota. An avid gardener with an eye for design, Matt is driven by an instinct to leave the world in better shape than he found it.',
+    photo: Wildenauer
   },
   {
     fullName: 'Ean Koerner',
     title: 'Founder & Director',
-    longBio: 'Ean’s tenure working for, and volunteering with not-for-profit organizations has spanned many years. As Marketing & Communications Specialist at Nativity of Mary Catholic Church & School, Ean is afforded countless opportunities to develop and execute projects that educate, advertise, fundraise and recruit. In addition to volunteer work with the St. Vincent de Paul Society, The Heritage Days Festival, youth baseball (coach for Bloomington Athletic Association and the Academy of Holy Angels), Ean is a co-founder of The Wildflower Project.',
+    longBio:
+      'Ean’s tenure working for, and volunteering with not-for-profit organizations has spanned many years. As Marketing & Communications Specialist at Nativity of Mary Catholic Church & School, Ean is afforded countless opportunities to develop and execute projects that educate, advertise, fundraise and recruit. In addition to volunteer work with the St. Vincent de Paul Society, The Heritage Days Festival, youth baseball (coach for Bloomington Athletic Association and the Academy of Holy Angels), Ean is a co-founder of The Wildflower Project.',
     photo: Koerner
   }
 ]
@@ -25,25 +27,22 @@ export const board = [
   {
     fullName: 'Jonathan Stowell',
     title: 'President',
-    longBio: 'Jon is a National Account Manager and Business Development officer at Commercial Partners Title based in Minneapolis, Minnesota. An alumnus of the University of Minnesota, Jon has a background in finance, business administration, and commercial real estate development, and has served in leadership capacities for several non-profit groups. Jon has served in officer roles in the Cook, MN and Stewartville, MN Chamber of Commerce and has been an organizer and fundraiser for the Minneapolis, MN based One Village Partners that coordinates development work in rural Sierra Leone. He is a 2008 recipient of the Leadership in Support of Neighborhoods grant for the Hamline-Midway neighborhood in St. Paul, MN.',
-    photo: Jon 
+    longBio:
+      'Jon is a National Account Manager and Business Development officer at Commercial Partners Title based in Minneapolis, Minnesota. An alumnus of the University of Minnesota, Jon has a background in finance, business administration, and commercial real estate development, and has served in leadership capacities for several non-profit groups. Jon has served in officer roles in the Cook, MN and Stewartville, MN Chamber of Commerce and has been an organizer and fundraiser for the Minneapolis, MN based One Village Partners that coordinates development work in rural Sierra Leone. He is a 2008 recipient of the Leadership in Support of Neighborhoods grant for the Hamline-Midway neighborhood in St. Paul, MN.',
+    photo: Jon
   },
   {
     fullName: 'Parker Anderson',
     title: 'Vice President',
-    longBio: 'Parker is a Research Scientist at the University of Minnesota. Parker facilitates the Science of the Green Initiative, a research lab focused on golf’s relationship to the environment and society and how to improve that relationship. Parker has Master of Landscape Architecture and Master of Science degrees from the University of Michigan and is a member of the Professional Golfers’ Association of America, the Golf Course Superintendents’ Association of America, and the American Society of Landscape Architects. Parker is a certified permaculture designer and has a passion for beekeeping and promoting the importance of pollinators through education and outreach.',
-    photo: Parker 
-  },
-  {
-    fullName: 'Megan Dobratz',
-    title: 'Secretary',
-    longBio: 'Megan is President of Native Sustainability, a creative consulting firm that develops innovative front-end solutions to long-term sustainability challenges, manages projects, leverages solutions to maximize social incentives, economic benefits and good will, while linking the work to a company’s overall mission and environmental goals. Megan has worked in the environmental field for more than 15 years, and brings a breadth of knowledge and network connections. Her work has included engagements with non-profit, for-profits, government entities and tribes. She has secured and managed state and national grant funds, organized large-scale events and simultaneously run two small businesses.',
-    photo: Megan 
+    longBio:
+      'Parker is a Research Scientist at the University of Minnesota. Parker facilitates the Science of the Green Initiative, a research lab focused on golf’s relationship to the environment and society and how to improve that relationship. Parker has Master of Landscape Architecture and Master of Science degrees from the University of Michigan and is a member of the Professional Golfers’ Association of America, the Golf Course Superintendents’ Association of America, and the American Society of Landscape Architects. Parker is a certified permaculture designer and has a passion for beekeeping and promoting the importance of pollinators through education and outreach.',
+    photo: Parker
   },
   {
     fullName: 'Matthew Peterson',
     title: 'Co-Founder & Web Development',
-    longBio: "A good friend of Ean Koerner and Matt Wildenauer's, Matthew joined TWP in 2015 as business developer and brought to fruition TWP's first few partnership gardens. Having moved to Utah, his role has changed to Web Developer (he built this website!), board member, and long-distance support.",
-    photo: Peterson 
-  },
+    longBio:
+      "A good friend of Ean Koerner and Matt Wildenauer's, Matthew joined TWP in 2015 as business developer and brought to fruition TWP's first few partnership gardens. Having moved to Utah, his role has changed to Web Developer (he built this website!), board member, and long-distance support.",
+    photo: Peterson
+  }
 ]
