5519a64832ecdfaed2461daf159e5c7bbd250770
Matthew Peterson <mbpeterson19@gmail.com>
2019-07-03 11:45:45 -0600

    whatever

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
-import ContactUsSection from "./Contact Us/ContactUsSection";
+import ContactUsSection from './Contact Us/ContactUsSection'
 
 import {
   TWPHeader,
@@ -10,107 +10,107 @@ import {
   TWPImage,
   TWPAnchor,
   media
-} from "../styles/GenericStyledComponents";
-import TWPStyleGuide from "../styles/TWPStyleGuide";
+} from '../styles/GenericStyledComponents'
+import TWPStyleGuide from '../styles/TWPStyleGuide'
 
-import whiteFlower from "../assets/images/basicWhiteFlower.png";
-import logo from "../assets/images/logoFullSize.png";
+import whiteFlower from '../assets/images/basicWhiteFlower.png'
+import logo from '../assets/images/logoFullSize.png'
 
 const FooterWrap = TWPDiv.extend`
   ${media.tablet`
     flex-direction: column;
     height: unset;
   `}
-`;
+`
 
 const FooterDuo = TWPDiv.extend`
   ${media.tablet`
     width: 100%;
     height: 200px;
   `}
-`;
+`
 
-const QuicklinksWrap = TWPDiv.extend``;
+const QuicklinksWrap = TWPDiv.extend``
 
 const Footer = () => {
   return (
     <div>
       <ContactUsSection />
-      <FooterWrap height={"200px"} width={"100%"} flexDirection={"row"} padding={"0"}>
+      <FooterWrap height={'200px'} width={'100%'} flexDirection={'row'} padding={'0'}>
         <FooterDuo
-          height={"100%"}
+          height={'100%'}
           backgroundColor={TWPStyleGuide.color.darkGreen}
-          width={"50%"}
-          justifyContent={"space-around"}
-          flexDirection={"row"}
+          width={'50%'}
+          justifyContent={'space-around'}
+          flexDirection={'row'}
         >
-          <QuicklinksWrap width={"85%"} height={"100%"} justifyContent={"space-around"}>
-            <TWPDiv width={"100%"}>
+          <QuicklinksWrap width={'85%'} height={'100%'} justifyContent={'space-around'}>
+            <TWPDiv width={'100%'}>
               <TWPHeader
                 color={TWPStyleGuide.color.white}
-                width={"fit-content"}
-                fontWeight={"100"}
+                width={'fit-content'}
+                fontWeight={'100'}
                 tabletFontSize={TWPStyleGuide.font.size.mediumLarge}
               >
                 Quick Links
               </TWPHeader>
             </TWPDiv>
-            <TWPDiv flexDirection={"row"} width={"100%"} justifyContent={"space-around"}>
-              <TWPDiv width={"50%"}>
+            <TWPDiv flexDirection={'row'} width={'100%'} justifyContent={'space-around'}>
+              <TWPDiv width={'50%'}>
                 <TWPAnchor
-                  textAlign={"left"}
+                  textAlign={'left'}
                   color={TWPStyleGuide.color.white}
-                  width={"100%"}
-                  href={"/mission"}
+                  width={'100%'}
+                  href={'/mission'}
                 >
                   About Us
                 </TWPAnchor>
                 <TWPAnchor
-                  textAlign={"left"}
+                  textAlign={'left'}
                   color={TWPStyleGuide.color.white}
-                  width={"100%"}
-                  href={"/contact"}
+                  width={'100%'}
+                  href={'/contact'}
                 >
                   Contact
                 </TWPAnchor>
                 <TWPAnchor
-                  textAlign={"left"}
+                  textAlign={'left'}
                   color={TWPStyleGuide.color.white}
-                  width={"100%"}
-                  href={"/volunteer"}
+                  width={'100%'}
+                  href={'/volunteer'}
                 >
                   Volunteer
                 </TWPAnchor>
               </TWPDiv>
-              <TWPDiv width={"50%"}>
+              <TWPDiv width={'50%'}>
                 <TWPAnchor
-                  textAlign={"left"}
+                  textAlign={'left'}
                   color={TWPStyleGuide.color.white}
-                  width={"100%"}
-                  href={"/partnerships"}
+                  width={'100%'}
+                  href={'/partnerships'}
                 >
                   Partnerships
                 </TWPAnchor>
                 <TWPAnchor
-                  textAlign={"left"}
+                  textAlign={'left'}
                   color={TWPStyleGuide.color.white}
-                  width={"100%"}
-                  href={"/Donate"}
+                  width={'100%'}
+                  href={'/Donate'}
                 >
                   Donate
                 </TWPAnchor>
                 <TWPAnchor
-                  textAlign={"left"}
+                  textAlign={'left'}
                   color={TWPStyleGuide.color.white}
-                  width={"100%"}
-                  href={"/photogallery"}
+                  width={'100%'}
+                  href={'/photogallery'}
                 >
                   Photo Gallery
                 </TWPAnchor>
               </TWPDiv>
             </TWPDiv>
           </QuicklinksWrap>
-          <TWPDiv width={"initial"} padding={"0"}>
+          <TWPDiv width={'initial'} padding={'0'}>
             <TWPImage
               width='unset'
               height='unset'
@@ -120,8 +120,8 @@ const Footer = () => {
             />
           </TWPDiv>
         </FooterDuo>
-        <FooterDuo width={"50%"} height={"100%"} justifyContent={"space-around"}>
-          <TWPDiv flexDirection={"row"} height={"60%"}>
+        <FooterDuo width={'50%'} height={'100%'} justifyContent={'space-around'}>
+          <TWPDiv flexDirection={'row'} height={'60%'}>
             <TWPDiv>
               <TWPImage
                 width='unset'
@@ -131,27 +131,27 @@ const Footer = () => {
                 src={logo}
               />
             </TWPDiv>
-            <TWPDiv width={"initial"}>
-              <TWPParagraph color={TWPStyleGuide.color.darkGreen} width={"fit-content"}>
+            <TWPDiv width={'initial'}>
+              <TWPParagraph color={TWPStyleGuide.color.darkGreen} width={'fit-content'}>
                 887 Raymond Ave.
                 <br />
                 Saint Paul, MN 55114
               </TWPParagraph>
             </TWPDiv>
           </TWPDiv>
-          <TWPDiv width={"100%"}>
+          <TWPDiv width={'100%'}>
             <TWPParagraph
-              width={"fit-content"}
+              width={'fit-content'}
               tabletFontSize={TWPStyleGuide.font.size.tiny}
-              padding={"2%"}
+              padding={'2%'}
             >
-              &copy; COPYRIGHT 2018. ALL RIGHTS RESERVED
+              &copy; COPYRIGHT 2019. ALL RIGHTS RESERVED
             </TWPParagraph>
           </TWPDiv>
         </FooterDuo>
       </FooterWrap>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
--- client/src/components/Home/TwoInfoBoxes.js
+++ client/src/components/Home/TwoInfoBoxes.js
@@ -1,71 +1,73 @@
-import React from "react";
+import React from 'react'
 import {
   TWPDiv,
   TWPSectionHeader,
   TWPImage,
   TWPParagraph,
   media
-} from "../../styles/GenericStyledComponents";
-import TWPStyleGuide from "../../styles/TWPStyleGuide";
-import Bouquet from "../../assets/images/BouquetOranges.png";
+} from '../../styles/GenericStyledComponents'
+import TWPStyleGuide from '../../styles/TWPStyleGuide'
+import Bouquet from '../../assets/images/BouquetOranges.png'
 
-import squares from "../../assets/images/Squares.png";
+import squares from '../../assets/images/Squares.png'
 
 export default function TwoInfoBoxes() {
   return (
     <TopWrapper
-      flexDirection={"row"}
-      width={"100%"}
-      backgroundColor={TWPStyleGuide.color.lightGreen}
+      flexDirection={'row'}
+      width={'100%'}
+      background={
+        'linear-gradient(to right, #92d03d 0%,#ffffff 40%,#ffffff 56%,#92d03d 100%)'
+      }
       justifyContent='space-around'
     >
       <TopTwoInfoBoxes
-        background={"linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))"}
+        background={'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'}
         backgroundColor={
-          "linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))"
+          'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'
         }
         desktopImage={null}
-        borderRadius={"5px"}
-        justifyContent={"space-around"}
-        flex={"1 1 50%"}
-        margin={"10px 0"}
-        width={"45%"}
+        borderRadius={'5px'}
+        justifyContent={'space-around'}
+        flex={'1 1 50%'}
+        margin={'10px 0'}
+        width={'45%'}
         height='95%'
         padding='0'
         maxWidth='500px'
         minWidth='330px'
       >
-        <TopTwoTopText padding={"5% 2%"} color={TWPStyleGuide.color.darkGreen}>
+        <TopTwoTopText padding={'5% 2%'} color={TWPStyleGuide.color.darkGreen}>
           Installing wildflower gardens in urban environments and underutilized or vacant
           spaces
         </TopTwoTopText>
         <TWPDiv
-          flexDirection={"row"}
-          width={"90%"}
+          flexDirection={'row'}
+          width={'90%'}
           backgroundColor={TWPStyleGuide.color.white}
-          margin={"2%"}
-          borderRadius={"3px"}
-          height={"40%"}
-          alignItems={"center"}
-          justifyContent={"space-around"}
+          margin={'2%'}
+          borderRadius={'3px'}
+          height={'40%'}
+          alignItems={'center'}
+          justifyContent={'space-around'}
         >
-          <TopTwoClipArt src={squares} height={"unset"} width={"25%"} />
-          <TopTwoBottomLeftNumberText alignItems={"flex-start"} width={"fit-content"}>
+          <TopTwoClipArt src={squares} height={'unset'} width={'25%'} />
+          <TopTwoBottomLeftNumberText alignItems={'flex-start'} width={'fit-content'}>
             <TopTwoNumbers
               color={TWPStyleGuide.color.brown}
               fontSize={TWPStyleGuide.font.size.mediumLarge}
-              textAlign={"left"}
-              padding={"0"}
-              width={"40%"}
+              textAlign={'left'}
+              padding={'0'}
+              width={'40%'}
               tabletFontSize={TWPStyleGuide.font.size.medium}
             >
               11,124
             </TopTwoNumbers>
             <TWPParagraph
-              padding={"0"}
+              padding={'0'}
               color={TWPStyleGuide.color.brown}
-              textAlign={"left"}
-              width={"fit-content"}
+              textAlign={'left'}
+              width={'fit-content'}
               tabletFontSize={TWPStyleGuide.font.size.smallTiny}
             >
               Square Footage Restored
@@ -74,56 +76,56 @@ export default function TwoInfoBoxes() {
         </TWPDiv>
       </TopTwoInfoBoxes>
       <TopTwoInfoBoxes
-        background={"linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))"}
+        background={'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'}
         backgroundColor={
-          "linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))"
+          'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'
         }
         desktopImage={null}
-        borderRadius={"5px"}
-        justifyContent={"space-around"}
-        flex={"1 1 50%"}
-        margin={"10px 0"}
-        width={"45%"}
+        borderRadius={'5px'}
+        justifyContent={'space-around'}
+        flex={'1 1 50%'}
+        margin={'10px 0'}
+        width={'45%'}
         height='95%'
         padding='0'
         maxWidth='500px'
         minWidth='330px'
       >
-        <TopTwoTopText padding={"5% 2%"} color={TWPStyleGuide.color.darkGreen}>
+        <TopTwoTopText padding={'5% 2%'} color={TWPStyleGuide.color.darkGreen}>
           Bringing vibrant, natural beauty to urban communities
         </TopTwoTopText>
         <TWPDiv
-          width={"90%"}
+          width={'90%'}
           backgroundColor={TWPStyleGuide.color.white}
-          flexDirection={"row"}
-          margin={"2%"}
-          borderRadius={"3px"}
-          height={"40%"}
-          alignItems={"center"}
+          flexDirection={'row'}
+          margin={'2%'}
+          borderRadius={'3px'}
+          height={'40%'}
+          alignItems={'center'}
         >
           <TopTwoNumbers
             color={TWPStyleGuide.color.brown}
             fontSize={TWPStyleGuide.font.size.mediumLarge}
-            textAlign={"right"}
-            width={"fit-content"}
+            textAlign={'right'}
+            width={'fit-content'}
             tabletFontSize={TWPStyleGuide.font.size.medium}
           >
             20
           </TopTwoNumbers>
           <TWPParagraph
             color={TWPStyleGuide.color.brown}
-            width={"fit-content"}
+            width={'fit-content'}
             tabletFontSize={TWPStyleGuide.font.size.smallTiny}
           >
             Gardens Planted
             <br />
             in the Twin Cities
           </TWPParagraph>
-          <TopTwoClipArt src={Bouquet} height={"unset"} width={"30%"} />
+          <TopTwoClipArt src={Bouquet} height={'unset'} width={'30%'} />
         </TWPDiv>
       </TopTwoInfoBoxes>
     </TopWrapper>
-  );
+  )
 }
 
 const TopTwoInfoBoxes = TWPDiv.extend`
@@ -140,13 +142,13 @@ const TopTwoInfoBoxes = TWPDiv.extend`
   @media(max-width: 500px) {
     width: 90%;
   }
-`;
+`
 
 const TopWrapper = TWPDiv.extend`
   @media (max-width: 1025px) {
     flex-direction: column;
   }
-`;
+`
 
 const TopTwoTopText = TWPSectionHeader.extend`
   height: 60%;
@@ -156,7 +158,7 @@ const TopTwoTopText = TWPSectionHeader.extend`
   ${media.phone`
     font-size: ${TWPStyleGuide.font.size.small};
   `}
-`;
+`
 
 const TopTwoClipArt = TWPImage.extend`
   max-height: 72px;
@@ -164,16 +166,16 @@ const TopTwoClipArt = TWPImage.extend`
   ${media.tablet`
     width: 25%
   `}
-`;
+`
 
 const TopTwoNumbers = TWPSectionHeader.extend`
   @media (max-width: 450px) {
     font-size: 2em;
   }
-`;
+`
 
 const TopTwoBottomLeftNumberText = TWPDiv.extend`
   @media (max-width: 450px) {
     padding: 0 6%;
   }
-`;
+`
