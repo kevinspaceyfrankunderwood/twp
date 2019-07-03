71ff0d66e7d86fa75595c052535c2fd4f216ee17
Matthew Peterson <mbpeterson19@gmail.com>
2019-07-03 10:23:12 -0600

    asdf

--- client/src/components/Home/AboveTheFold.js
+++ client/src/components/Home/AboveTheFold.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 import {
   TWPHeader,
@@ -8,21 +8,21 @@ import {
   TWPImage,
   TWPAnchor,
   media
-} from "../../styles/GenericStyledComponents";
-import TWPStyleGuide from "../../styles/TWPStyleGuide";
+} from '../../styles/GenericStyledComponents'
+import TWPStyleGuide from '../../styles/TWPStyleGuide'
 
-import Field from "../../assets/images/FlowerField.jpg";
-import squares from "../../assets/images/Squares.png";
-import Bouquet from "../../assets/images/BouquetOranges.png";
-import urban from "../../assets/images/urbanBoys.jpg";
-import sunrise from "../../assets/images/sunriseLiatris.jpg";
-import Banner from "../../assets/images/SummerBuzz_Banner.jpg";
+import Field from '../../assets/images/FlowerField.jpg'
+import squares from '../../assets/images/Squares.png'
+import Bouquet from '../../assets/images/BouquetOranges.png'
+import urban from '../../assets/images/urbanBoys.jpg'
+import butterfly from '../../assets/images/SideBoobMonarch.jpg'
+import Banner from '../../assets/images/SummerBuzz_Banner.jpg'
 
 const AboveTheFoldWrap = TWPDiv.extend`
   ${media.tablet`
     margin-top: 100px;
   `}
-`;
+`
 
 const TopTwoInfoBoxes = TWPDiv.extend`
   ${media.desktop`
@@ -35,13 +35,13 @@ const TopTwoInfoBoxes = TWPDiv.extend`
   @media(max-width: 500px) {
     width: 90%;
   }
-`;
+`
 
 const TopWrapper = TWPDiv.extend`
   ${media.tablet`
     flex-direction: column;
   `}
-`;
+`
 
 const TopTwoTopText = TWPSectionHeader.extend`
   height: 60%;
@@ -51,7 +51,7 @@ const TopTwoTopText = TWPSectionHeader.extend`
   ${media.phone`
     font-size: ${TWPStyleGuide.font.size.small};
   `}
-`;
+`
 
 const TopTwoClipArt = TWPImage.extend`
   max-height: 72px;
@@ -59,54 +59,50 @@ const TopTwoClipArt = TWPImage.extend`
   ${media.tablet`
     width: 25%
   `}
-`;
+`
 
 const TopTwoNumbers = TWPSectionHeader.extend`
   @media (max-width: 450px) {
     font-size: 2em;
   }
-`;
+`
 
 const TopTwoBottomLeftNumberText = TWPDiv.extend`
   @media (max-width: 450px) {
     padding: 0 6%;
   }
-`;
+`
 
 export const AboveTheFold = () => {
   return (
-    <AboveTheFoldWrap padding={"0"}>
-      <TWPDiv>
+    <AboveTheFoldWrap padding={'0'}>
+      {/* <TWPDiv>
         <TWPImage
           height='unset'
           width='100%'
           maxHeight='unset'
           maxWidth='800px'
-          src={Banner}
+          src={sunrise}
         />
-      </TWPDiv>
-      {/* <TWPDiv
-        background={`linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, .8), rgba(255, 255, 255, 0)), url(${sunrise})`}
+      </TWPDiv> */}
+      <TWPDiv
+        background={`linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, .8), rgba(255, 255, 255, 0)), url(${butterfly})`}
         backgroundSize={'cover'}
         backgroundPosition={'top'}
         backgroundAttachment={'fixed'}
         height={'650px'}
         width={'100%'}
       >
-        <TWPDiv
-          height={'25%'} 
-          width={'100%'}
-        >
-          <TWPHeader 
+        <TWPDiv height={'25%'} width={'100%'}>
+          <TWPHeader
             fontWeight={'300'}
             width={'100%'}
             tabletFontSize={TWPStyleGuide.font.size.mediumLarge}
-            mobileFontSize={TWPStyleGuide.font.size.medium
-            }
+            mobileFontSize={TWPStyleGuide.font.size.medium}
           >
-            <span style={{color: TWPStyleGuide.color.yellow}}>the</span>
-            <span style={{color: TWPStyleGuide.color.brightOrange}}>wildflower</span>
-            <span style={{color: TWPStyleGuide.color.darkGreen}}>project</span>
+            <span style={{ color: 'rgb(255, 251, 0)' }}>the</span>
+            <span style={{ color: TWPStyleGuide.color.brightOrange }}>wildflower</span>
+            <span style={{ color: TWPStyleGuide.color.darkGreen }}>project</span>
           </TWPHeader>
           <TWPSectionHeader
             fontWeight={'300'}
@@ -116,14 +112,14 @@ export const AboveTheFold = () => {
             planting with a purpose
           </TWPSectionHeader>
         </TWPDiv>
-        <TopWrapper 
-          flexDirection={'row'}
-          width={'100%'}
-          height={'75%'}
-        >
-          <TopTwoInfoBoxes 
-            background={'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'}
-            backgroundColor={'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'}
+        {/* <TopWrapper flexDirection={'row'} width={'100%'} height={'75%'}>
+          <TopTwoInfoBoxes
+            background={
+              'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'
+            }
+            backgroundColor={
+              'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'
+            }
             desktopImage={null}
             borderRadius={'5px'}
             justifyContent={'space-between'}
@@ -131,15 +127,13 @@ export const AboveTheFold = () => {
             margin={'0 5%'}
             height={'45%'}
           >
-            <TopTwoTopText 
-              padding={'5% 2%'}
-              color={TWPStyleGuide.color.darkGreen}
-            >
-              Installing wildflower gardens in urban environments and underutilized or vacant spaces
+            <TopTwoTopText padding={'5% 2%'} color={TWPStyleGuide.color.darkGreen}>
+              Installing wildflower gardens in urban environments and underutilized or
+              vacant spaces
             </TopTwoTopText>
-            <TWPDiv 
-              flexDirection={'row'} 
-              width={'90%'} 
+            <TWPDiv
+              flexDirection={'row'}
+              width={'90%'}
               backgroundColor={TWPStyleGuide.color.white}
               margin={'2%'}
               borderRadius={'3px'}
@@ -147,17 +141,10 @@ export const AboveTheFold = () => {
               alignItems={'center'}
               justifyContent={'space-around'}
             >
-              <TopTwoClipArt
-                src={squares}
-                height={'unset'}
-                width={'25%'}
-              />
-              <TopTwoBottomLeftNumberText
-                alignItems={'flex-start'}
-                width={'fit-content'}
-              >
-                <TopTwoNumbers 
-                  color={TWPStyleGuide.color.brown} 
+              <TopTwoClipArt src={squares} height={'unset'} width={'25%'} />
+              <TopTwoBottomLeftNumberText alignItems={'flex-start'} width={'fit-content'}>
+                <TopTwoNumbers
+                  color={TWPStyleGuide.color.brown}
                   fontSize={TWPStyleGuide.font.size.mediumLarge}
                   textAlign={'left'}
                   padding={'0'}
@@ -167,8 +154,8 @@ export const AboveTheFold = () => {
                   11,124
                 </TopTwoNumbers>
                 <TWPParagraph
-                  padding={'0'} 
-                  color={TWPStyleGuide.color.brown} 
+                  padding={'0'}
+                  color={TWPStyleGuide.color.brown}
                   textAlign={'left'}
                   width={'fit-content'}
                   tabletFontSize={TWPStyleGuide.font.size.smallTiny}
@@ -178,9 +165,13 @@ export const AboveTheFold = () => {
               </TopTwoBottomLeftNumberText>
             </TWPDiv>
           </TopTwoInfoBoxes>
-          <TopTwoInfoBoxes 
-            background={'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'}
-            backgroundColor={'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'}
+          <TopTwoInfoBoxes
+            background={
+              'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'
+            }
+            backgroundColor={
+              'linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7))'
+            }
             desktopImage={null}
             borderRadius={'5px'}
             justifyContent={'space-around'}
@@ -188,14 +179,11 @@ export const AboveTheFold = () => {
             margin={'0 5%'}
             height={'45%'}
           >
-            <TopTwoTopText 
-              padding={'5% 2%'}
-              color={TWPStyleGuide.color.darkGreen}
-            >
+            <TopTwoTopText padding={'5% 2%'} color={TWPStyleGuide.color.darkGreen}>
               Bringing vibrant, natural beauty to urban communities
             </TopTwoTopText>
-            <TWPDiv 
-              width={'90%'} 
+            <TWPDiv
+              width={'90%'}
               backgroundColor={TWPStyleGuide.color.white}
               flexDirection={'row'}
               margin={'2%'}
@@ -203,8 +191,8 @@ export const AboveTheFold = () => {
               height={'40%'}
               alignItems={'center'}
             >
-              <TopTwoNumbers 
-                color={TWPStyleGuide.color.brown} 
+              <TopTwoNumbers
+                color={TWPStyleGuide.color.brown}
                 fontSize={TWPStyleGuide.font.size.mediumLarge}
                 textAlign={'right'}
                 width={'fit-content'}
@@ -212,24 +200,22 @@ export const AboveTheFold = () => {
               >
                 20
               </TopTwoNumbers>
-              <TWPParagraph 
-                color={TWPStyleGuide.color.brown} 
+              <TWPParagraph
+                color={TWPStyleGuide.color.brown}
                 width={'fit-content'}
                 tabletFontSize={TWPStyleGuide.font.size.smallTiny}
               >
-                Gardens Planted<br/>in the Twin Cities
+                Gardens Planted
+                <br />
+                in the Twin Cities
               </TWPParagraph>
-              <TopTwoClipArt
-                src={Bouquet}
-                height={'unset'}
-                width={'30%'}
-              />
+              <TopTwoClipArt src={Bouquet} height={'unset'} width={'30%'} />
             </TWPDiv>
           </TopTwoInfoBoxes>
-        </TopWrapper>
-      </TWPDiv> */}
+        </TopWrapper> */}
+      </TWPDiv>
     </AboveTheFoldWrap>
-  );
-};
+  )
+}
 
-export default AboveTheFold;
+export default AboveTheFold
